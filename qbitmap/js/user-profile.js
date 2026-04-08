4d2daa7be848df53966536d50bf597a5b12ddac2
eUGENE-BNX <alp@qbitwise.com>
2026-04-08 10:27:33 +0300

    profile: add Tesla section title above Tesla card

--- qbitmap/js/user-profile.js
+++ qbitmap/js/user-profile.js
@@ -188,6 +188,7 @@ const UserProfileSystem = {
 
   renderTeslaConnectCard() {
     return `
+      <h4 class="profile-section-title">Tesla</h4>
       <div class="profile-tesla-section profile-tesla-cta">
         <div class="profile-tesla-cta-row">
           <div class="profile-tesla-logo">T</div>
@@ -242,6 +243,7 @@ const UserProfileSystem = {
     const telemetryOff = vehicle.telemetryEnabled === false;
 
     return `
+      <h4 class="profile-section-title">Tesla</h4>
       <div class="profile-tesla-section">
         ${telemetryOff ? `
           <div class="profile-tesla-banner" data-vehicle-id="${escapeHtml(vehicleId)}">
